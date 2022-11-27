c0045c52075d4c09db34b1dbf45f665342367625
houcem-benjeddou <92691885+houcem-benjeddou@users.noreply.github.com>
2022-11-27 23:02:09 +0100

    Add files via upload

---
+++ Alphonso Davies/src/alphonso/GestionEtudiant.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphonso;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author benje
+ */
+public interface GestionEtudiant {
+    public void afficherEtudiants(List<Etudiant> etudiants, Consumer<Etudiant> consumer);
+    public void afficherEtudiantsSelonFiltre(List<Etudiant> etudiants, Predicate<Etudiant> predicate, Consumer<Etudiant> consumer);
+    public String afficherNomEtudiant(List<Etudiant> etudiants, Function<Etudiant, String> function);
+    public Etudiant creeEtudiant(Supplier<Etudiant> supplier);   
+    public Stream convertListStream(List<Etudiant> etudiants);
+    
+}
